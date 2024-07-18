02e437a1c29288f5bd861b098a34144474c009d3
Rei Wolf <joseph.elkhouri+github@gmail.com>
2024-07-17 19:42:26 -0700

    working basic 2

---
+++ LycanBot/LycanBot.py
@@ -0,0 +1,20 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class LycanBot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        @self.bot.event
+        async def on_ready():
+            print(f'{bot.user} has connected to Discord! Version {discord.__version__}')
+            try:
+                synced = await bot.tree.sync()
+                print(f"Synced {len(synced)} commands.")
+            except Exception as e:
+                print(e)
+
+async def setup(bot):
+    await bot.add_cog(LycanBot(bot), guild=1263261495853056030)
+    await bot.load_extension('LycanBot.commands.bark')
---
+++ LycanBot/commands/bark.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+import discord
+
+class Bark(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command()
+    async def bark(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        await ctx.send(f'arrruf !! hi {member.name} !!!')
+
+async def setup(bot):
+    await bot.add_cog(Bark(bot), guild=1263261495853056030)
